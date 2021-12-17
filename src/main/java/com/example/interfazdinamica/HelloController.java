ab54a2d024fc01f513a043882e18bf4c96ee6d08
AlejandroLasoSalido <alex_laso_laso@hotmai.com>
2021-12-17 09:59:32 +0100

    -

--- src/main/java/com/example/interfazdinamica/HelloController.java
+++ src/main/java/com/example/interfazdinamica/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.interfazdinamica;
 
 import javafx.animation.TranslateTransition;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
@@ -14,7 +18,7 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
     private boolean isDesplegado;
-    private TranslateTransition animacion;
+    private TranslateTransition animacion, animacionbtn;
     @FXML
     private Button btnMenu;
     @FXML
@@ -22,24 +26,56 @@ public class HelloController implements Initializable {
     @FXML
     private StackPane fondo;
 
+    /*ejemplo StringProperty
+    private StringProperty prueba;
+    public String getPrueba() {return prueba.get();}
+    public StringProperty pruebaProperty() {return prueba;}
+    public void setPrueba(String prueba) {this.prueba.set(prueba);}*/
+
+
     //solo metodos o cosas que no tengan que ver con la vista
     public HelloController(){
-    isDesplegado=false;
+
 
     }
 
     // elementos de la vista
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        desplegable.translateXProperty().bind(desplegable.widthProperty());
-
-        initAnimacion();
+        desplegable.setTranslateX(-desplegable.getWidth());
+        isDesplegado=false;
     }
 
-    private void initAnimacion(){
-        animacion=new TranslateTransition(Duration.millis(17));
-        animacion.setFromX(-fondo.getWidth()-desplegable.getWidth());
-        animacion.setToX(-fondo.getWidth());
+
+    @FXML
+    protected void desplegarMenu(){
+        animacion=new TranslateTransition(Duration.millis(300),desplegable);
+        animacionbtn= new TranslateTransition(Duration.millis(300),btnMenu);
+
+        if(!isDesplegado){
+            animacion.setFromX(-desplegable.getWidth());
+            animacion.setToX(0);
+            animacionbtn.setFromX(0);
+            animacionbtn.setToX(desplegable.getWidth());
+            isDesplegado=true;
+
+        }else{
+            animacion.setFromX(0);
+            animacion.setToX(-desplegable.getWidth());
+            animacionbtn.setFromX(desplegable.getWidth());
+            animacionbtn.setToX(0);
+            isDesplegado=false;
+
+        }
         animacion.play();
+        animacionbtn.play();
     }
-}
+
+    @FXML
+    protected void vista1(){
+      //  fondo.setBackground(new BackgroundImage(new Image("perros.jpeg")));
+    }
+
+    @FXML
+    protected void vista2(){
+}}
